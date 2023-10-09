93e9a6f1574c42ccc5311efc3ada9e730c796ff5
jeromehaas <hello@jeromehaas.ch>
2023-10-09 16:29:33 +0200

    Delete: boilerplate code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+	images: {
+		domains: ['localhost', 'picsum.photos', 'samirahaas-web-development.fra1.digitaloceanspaces.com', 'samirahaas-web-production.fra1.digitaloceanspaces.com'],
+	},
+	i18n: {
+		locales: ['de-ch'],
+		defaultLocale: 'de-ch',
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
