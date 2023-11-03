b14864f281500b80043cfe559c9b40bbfc0ed235
jeromehaas <hello@jeromehaas.ch>
2023-11-03 22:10:03 +0100

    Refactor: add fallback for loop in impressions component

--- components/02-molecules/impressions/impressions.jsx
+++ components/02-molecules/impressions/impressions.jsx
@@ -10,7 +10,7 @@ const Component = ({ className, items }) => {
 			<div className="impressions__inner">
 				{ items.map((item) => (
 					<div className="impressions__item item" key={ item.id }>
-						{ item.group.data.map((image, index) => (
+						{ item.group.data?.map((image, index) => (
 							<Picture className="item__image animation--fade-in" src={ image.attributes.url } key={ `${ image.id } ` } quality={ 100 } alt="Image" width={ image.attributes.width } height={ image.attributes.height } priority={ false } />
 						))}
 					</div>
