d47944de7ac26416f27ee72b0f4a585df89eb365
jeromehaas <hello@jeromehaas.ch>
2023-11-08 08:08:56 +0100

    Refactor: metadata to disallow zoom

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ const metadata = {
 	metadataBase: new URL(process.env.NEXT_PUBLIC_UI_URI),
 	title: 'Samira Haas',
 	description: 'Als Fotografin und Gestalterin verbinde ich meine offene und kreative Denkweise mit meiner Empathie, um individuelle und hochwertige Projekte zu schaffen. Ich glaube daran, dass jeder Moment und jede Person einzigartig ist und einen eigenen Ausdruck verdient. Mit einem Auge für Ästhetik und Liebe zum Detail erschaffe ich einzigartige visuelle Geschichten.',
+	viewport: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
 	openGraph: {
 		title: 'Samira Haas',
 		description: 'Als Fotografin und Gestalterin verbinde ich meine offene und kreative Denkweise mit meiner Empathie, um individuelle und hochwertige Projekte zu schaffen. Ich glaube daran, dass jeder Moment und jede Person einzigartig ist und einen eigenen Ausdruck verdient. Mit einem Auge für Ästhetik und Liebe zum Detail erschaffe ich einzigartige visuelle Geschichten.',
