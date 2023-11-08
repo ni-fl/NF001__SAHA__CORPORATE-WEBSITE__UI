0e0d95be3c31bb5af42fe7493faec7b99214f57b
jeromehaas <hello@jeromehaas.ch>
2023-11-08 08:24:00 +0100

    Refactor: remove i18 config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ module.exports = {
 	images: {
 		domains: ['localhost', 'picsum.photos', 'samirahaas-web-development.fra1.digitaloceanspaces.com', 'samirahaas-web-production.fra1.digitaloceanspaces.com'],
 	},
-	i18n: {
-		locales: ['de-ch'],
-		defaultLocale: 'de-ch',
-	},
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
