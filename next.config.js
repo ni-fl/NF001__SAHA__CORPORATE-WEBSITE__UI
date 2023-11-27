41980150f6d2d74afedea6d175ca29b31c495d1a
jeromehaas <hello@jeromehaas.ch>
2023-11-27 22:22:34 +0100

    Refactor: add image domains for new space url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
 	images: {
-		domains: ['localhost', 'picsum.photos', 'nf001-saha-website-space-pro.fra1.cdn.digitaloceanspaces.com', 'nf001-saha-website-space-dev.fra1.cdn.digitaloceanspaces.com'],
+		domains: [
+			'localhost',
+			'picsum.photos',
+			'nf001-saha-website-space-pro.fra1.digitaloceanspaces.com',
+			'nf001-saha-website-space-pro.fra1.cdn.digitaloceanspaces.com',
+			'nf001-saha-website-space-dev.fra1.digitaloceanspaces.com',
+			'nf001-saha-website-space-dev.fra1.cdn.digitaloceanspaces.com',
+		],
 	},
 	eslint: {
 		ignoreDuringBuilds: true,
