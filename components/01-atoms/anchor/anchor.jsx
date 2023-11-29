d0a536911496b424b074e493095411ba2247d29e
jeromehaas <hello@jeromehaas.ch>
2023-11-29 08:35:47 +0100

    Refactor: change style of anchor

--- components/01-atoms/anchor/anchor.jsx
+++ components/01-atoms/anchor/anchor.jsx
@@ -4,7 +4,7 @@ const Component = ({ className = '', children = null, href = '#', onClick = null
 	// RENDER
 	return (
 		<a className={ ` ${ className } anchor` } href={ href } onClick={ onClick }>
-			<span className="anchor__content">
+			<span className="anchor__content heading heading--h3">
 				{ children }
 			</span>
 		</a>
