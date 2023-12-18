82e63aa1e026e926bd9105fcfe25acb94213ab3e
jeromehaas <hello@jeromehaas.ch>
2023-12-18 20:32:33 +0100

    Refactor: styles of references in gallery component

--- components/02-molecules/references/references.jsx
+++ components/02-molecules/references/references.jsx
@@ -1,5 +1,4 @@
 // IMPORTS
-import Heading from 'components/01-atoms/heading/heading';
 import Text from 'components/01-atoms/text/text';
 
 // COMPONENT
@@ -11,10 +10,10 @@ const Component = ({ className, items }) => {
 			<div className="references__inner">
 				{ items.map((item) => (
 					<div className="references__item item" key={ item.id }>
-						<Heading className="item__heading" level="h3">{ item.title }</Heading>
-						<span className="item__content content">
-							<Text className="content__name text">{ item.name }</Text>, <a className="content__anchor text" href={ item.anchor.link } target="_blank" rel="noreferrer">{ item.anchor.label }</a>
-						</span>
+						<Text className="item__heading text--bold" level="h3">{ item.title }</Text>
+						<Text className="item__name">{ item.name }</Text>
+						{ item.anchor.link && item.anchor.label && <Text className="item__separator text">,</Text>}
+						{ item.anchor.link && item.anchor.label && <a className="item__anchor text" href={ item.anchor.link } target="_blank" rel="noreferrer">{ item.anchor.label }</a> }
 					</div>
 				))}
 			</div>
