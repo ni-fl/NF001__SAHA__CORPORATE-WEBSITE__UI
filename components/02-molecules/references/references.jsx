45eb77f071abdfd7e42a0d1c34ce1ab6cf8db399
jeromehaas <hello@jeromehaas.ch>
2024-01-09 14:04:58 +0100

    Refactor: style of references in gallery component

--- components/02-molecules/references/references.jsx
+++ components/02-molecules/references/references.jsx
@@ -1,5 +1,6 @@
 // IMPORTS
 import Text from 'components/01-atoms/text/text';
+import Heading from 'components/01-atoms/heading/heading';
 
 // COMPONENT
 const Component = ({ className, items }) => {
