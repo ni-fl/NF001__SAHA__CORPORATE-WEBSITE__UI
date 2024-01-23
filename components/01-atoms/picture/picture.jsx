3f01d111064f97f6131d95afc659713bcb7ecd75
jeromehaas <hello@jeromehaas.ch>
2024-01-23 14:53:57 +0100

    Refactor: add priority flag to images in impressions

--- components/01-atoms/picture/picture.jsx
+++ components/01-atoms/picture/picture.jsx
@@ -8,7 +8,7 @@ const Component = ({ className, src, alt, quality, priority = true, width, heigh
 	return (
 		<div className={ `${ className } picture` }>
 			{ src ? (
-				<Image className="picture__source" src={ src } alt={ alt } width={ width || 2500 } height={ height || 1600 } quality={ quality || 100 } priority={ priority } />
+				<Image className="picture__source" src={ src } alt={ alt } width={ width || 2500 } height={ height || 1600 } quality={ quality || 100 } priority={ priority } loading="eager" />
 			) : null }
 		</div>
 	);
