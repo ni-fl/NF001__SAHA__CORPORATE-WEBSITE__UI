dcfed2fa0dd04215585203f4f73135e555837c71
jeromehaas <hello@jeromehaas.ch>
2024-01-24 14:03:22 +0100

    Fix: hydration error with lp-plugin

--- components/01-atoms/input-field/input-field.jsx
+++ components/01-atoms/input-field/input-field.jsx
@@ -2,9 +2,18 @@
 
 // IMPORTS
 import { User, Key } from 'react-feather';
+import { useEffect, useState } from 'react';
 
 // COMPONENT
 const InputField = ({ className = '', id = '', placeholder = '', type = 'text', icon = null, register = null, validation = null, error = null, defaultValue = null }) => {
+
+	// SETUP STATE
+	const [isClient, setIsClient] = useState(false);
+
+	// CHECK FOR CLIENT
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
 	// RENDER
 	return (
@@ -13,7 +22,7 @@ const InputField = ({ className = '', id = '', placeholder = '', type = 'text', 
 				{ icon === 'user' && <User className="label__icon" /> }
 				{ icon === 'key' && <Key className="label__icon" /> }
 			</div>
-			<input className="input-field__input text text--small" id={ id } type={ type } placeholder={ placeholder } { ...register(id, validation) } defaultValue={ defaultValue } />
+			{ isClient && <input className="input-field__input text text--small" id={ id } type={ type } placeholder={ placeholder } { ...register(id, validation) } defaultValue={ defaultValue } /> }
 		</div>
 	);
 
