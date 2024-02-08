f6588b7e83430da9308361ff9f1fc09d7eb9cf8c
jeromehaas <hello@jeromehaas.ch>
2024-02-08 09:54:38 +0100

    Setup: plausible provider

--- components/04-layouts/page/page.jsx
+++ components/04-layouts/page/page.jsx
@@ -3,16 +3,20 @@
 // IMPORTS
 import Navigation from 'components/03-organisms/navigation/navigation';
 import Main from 'components/03-organisms/main/main';
+import PlausibleProvider from 'next-plausible';
+
 
 // COMPONENT
 const Component = ({ className = '', children = null }) => {
 
 	// RENDER
 	return (
-		<div className={`${ className } page`}>
-			<Navigation />
-			<Main content={ children } />
-		</div>
+		<PlausibleProvider domain="samirahaas.ch">
+			<div className={`${ className } page`}>
+				<Navigation />
+				<Main content={ children } />
+			</div>
+		</PlausibleProvider>
 	);
 
 };
